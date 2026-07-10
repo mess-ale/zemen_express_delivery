cd570e6a2ea83dbe3feef1afcd0630bc306bf82c
Meseret Alemnew <84620197+mess-ale@users.noreply.github.com>
2026-07-10 11:07:15 +0300

    deploy on github hashroute

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/zemen_express_delivery/",
   plugins: [react(), tailwindcss()],
+  base: "/zemen_express_delivery/",
 });
